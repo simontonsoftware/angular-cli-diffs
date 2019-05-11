efa69e7581aa2cde102fc959e90a061f9a942401
ersimont <8042088+ersimont@users.noreply.github.com>
2019-05-11 10:36:25 -0400

    npx ng generate library the-library --skip-install --no-interactive

---
+++ the-project/projects/the-library/src/lib/the-library.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { TheLibraryComponent } from './the-library.component';
+
+@NgModule({
+  declarations: [TheLibraryComponent],
+  imports: [
+  ],
+  exports: [TheLibraryComponent]
+})
+export class TheLibraryModule { }
