ba43e18b9be9dc01e71b755c0e88b786e34517dd
ersimont <8042088+ersimont@users.noreply.github.com>
2019-11-09 12:54:39 -0500

    npx ng generate library the-library --skip-install --no-interactive

---
+++ the-project/projects/the-library/src/lib/the-library.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-the-library',
+  template: `
+    <p>
+      the-library works!
+    </p>
+  `,
+  styles: []
+})
+export class TheLibraryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
