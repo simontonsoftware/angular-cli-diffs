9ece8a8ea75c62baed8045c184614a41ff817a0c
ersimont <8042088+ersimont@users.noreply.github.com>
2020-04-03 19:22:22 -0400

    libraries and PWAs via flags

--- generate-version.ts
+++ generate-version.ts
@@ -1,27 +1,32 @@
 import * as rimraf from 'rimraf';
 import {execSync, ExecSyncOptions} from 'child_process';
 
-if (process.argv.length !== 3) {
-  console.log(process.argv);
-  throw new Error("Pass a single argument: the version to generate");
-}
-
-const version = process.argv[2];
-console.log('generating version', version);
-
 const projectName = 'the-project';
 const libraryName = 'the-library';
 
-execSync(`git checkout -b ${version}`);
+const version = process.argv[2];
+const flags = new Set(process.argv.slice(3));
+const branch = version + Array.from(flags).sort().join('');
+console.log('generating', branch);
+
+execSync(`git checkout -b ${branch}`);
 
 rimraf.sync(projectName);
 
 runAndCommit(`yarn add -D @angular/cli@${version}`);
 runAndCommit(`npx ng new ${projectName} --skip-install --no-interactive`);
-runAndCommit(
-  `npx ng generate library ${libraryName} --skip-install --no-interactive`,
-  { cwd: projectName },
-);
+if (flags.has('-lib')) {
+  runAndCommit(
+    `npx ng generate library ${libraryName} --skip-install --no-interactive`,
+    { cwd: projectName },
+  );
+}
+if (flags.has('-pwa')) {
+  runAndCommit(
+    `npx ng add @angular/pwa --project ${projectName}`,
+    { cwd: projectName },
+  );
+}
 
 function runAndCommit(command: string, options?: ExecSyncOptions) {
   execSync(command, options);
