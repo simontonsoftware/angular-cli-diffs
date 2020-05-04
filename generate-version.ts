64777b56cdf52b921571215830ba987d49ea254b
ersimont <8042088+ersimont@users.noreply.github.com>
2020-05-04 06:43:54 -0400

    add routing option

--- generate-version.ts
+++ generate-version.ts
@@ -1,36 +1,44 @@
-import * as rimraf from 'rimraf';
-import {execSync, ExecSyncOptions} from 'child_process';
+import * as rimraf from "rimraf";
+import { execSync, ExecSyncOptions } from "child_process";
 
-const projectName = 'the-project';
-const libraryName = 'the-library';
+const projectName = "the-project";
+const libraryName = "the-library";
 
 const version = process.argv[2];
 const flags = new Set(process.argv.slice(3));
-const branch = version + Array.from(flags).sort().join('');
-console.log('generating', branch);
+const branch = version + Array.from(flags).sort().join("");
+console.log("generating", branch);
 
 execSync(`git checkout -b ${branch}`);
 
 rimraf.sync(projectName);
 
 runAndCommit(`yarn add -D @angular/cli@${version}`);
-runAndCommit(`npx ng new ${projectName} --skip-install --no-interactive`);
-if (flags.has('-lib')) {
+runAndCommit(
+  `npx ng new ${projectName} --routing=${flags.has(
+    "-route"
+  )} --skip-install --no-interactive`
+);
+if (flags.has("-lib")) {
   runAndCommit(
     `npx ng generate library ${libraryName} --skip-install --no-interactive`,
-    { cwd: projectName },
+    { cwd: projectName }
   );
 }
-if (flags.has('-mat')) {
-  runAndCommit('npx ng add @angular/material --interactive=false', { cwd: projectName });
+if (flags.has("-mat")) {
+  runAndCommit("npx ng add @angular/material --interactive=false", {
+    cwd: projectName,
+  });
 }
-if (flags.has('-pwa')) {
-  runAndCommit('npx ng add @angular/pwa --interactive=false', { cwd: projectName });
+if (flags.has("-pwa")) {
+  runAndCommit("npx ng add @angular/pwa --interactive=false", {
+    cwd: projectName,
+  });
 }
 rimraf.sync(`${projectName}/node_modules`);
 
 function runAndCommit(command: string, options?: ExecSyncOptions) {
   execSync(command, options);
-  execSync('git add .');
+  execSync("git add .");
   execSync(`git commit -m "${command}"`);
 }
