78cd763364f16447c563a8df0079af2347a98ce0
ersimont <8042088+ersimont@users.noreply.github.com>
2021-05-13 15:09:36 -0400

    add the `-eslint` flag

--- generate-version.ts
+++ generate-version.ts
@@ -1,5 +1,7 @@
-import * as rimraf from "rimraf";
 import { execSync, ExecSyncOptions } from "child_process";
+import * as rimraf from "rimraf";
+
+const runCommands = true;
 
 const projectName = "the-project";
 const applicationName = "the-application";
@@ -8,41 +10,45 @@ const libraryName = "the-library";
 const version = process.argv[2];
 const flags = new Set(process.argv.slice(3));
 const branch = version + Array.from(flags).sort().join("");
-console.log("generating", branch);
 
-execSync(`git checkout -b ${branch}`);
-
+run(`git checkout -b ${branch}`, {});
 rimraf.sync(projectName);
-
-runAndCommit(`yarn add -D @angular/cli@${version}`);
+runAndCommit(`yarn add -D @angular/cli@${version}`, {});
 runAndCommit(
   `npx ng new ${projectName} --createApplication=${!flags.has(
     "-noApp"
   )} --routing=${flags.has("-route")} --strict=${flags.has(
     "-strict"
-  )} --skip-install --no-interactive --packageManager=yarn --style=scss`
+  )} --skip-install --interactive=false --packageManager=yarn --style=scss`,
+  {}
 );
+if (flags.has("-eslint")) {
+  runAndCommit(
+    `npx ng add @angular-eslint/schematics --interactive=false --skip-confirmation=true`
+  );
+  if (flags.has("-noApp")) {
+    runAndCommit(`ng config cli.defaultCollection @angular-eslint/schematics`);
+  }
+}
 if (flags.has("-subApp")) {
   runAndCommit(
-    `npx ng generate application ${applicationName} --skip-install --no-interactive`,
-    { cwd: projectName }
+    `npx ng generate application ${applicationName} --skip-install --interactive=false`
   );
 }
 if (flags.has("-lib")) {
   runAndCommit(
-    `npx ng generate library ${libraryName} --skip-install --no-interactive`,
-    { cwd: projectName }
+    `npx ng generate library ${libraryName} --skip-install --interactive=false`
   );
 }
 if (flags.has("-mat")) {
-  runAndCommit("npx ng add @angular/material --interactive=false --skip-confirmation=true", {
-    cwd: projectName,
-  });
+  runAndCommit(
+    "npx ng add @angular/material --interactive=false --skip-confirmation=true"
+  );
 }
 if (flags.has("-pwa")) {
-  runAndCommit("npx ng add @angular/pwa --interactive=false --skip-confirmation=true", {
-    cwd: projectName,
-  });
+  runAndCommit(
+    "npx ng add @angular/pwa --interactive=false --skip-confirmation=true"
+  );
 }
 if (flags.has("-fire")) {
   console.error("@angular/fire must be added manually. Run this and commit:");
@@ -50,8 +56,22 @@ if (flags.has("-fire")) {
 }
 rimraf.sync(`${projectName}/node_modules`);
 
-function runAndCommit(command: string, options?: ExecSyncOptions) {
-  execSync(command, options);
-  execSync("git add .");
-  execSync(`git commit -m "${command}"`);
+function runAndCommit(
+  command: string,
+  options: ExecSyncOptions = { cwd: projectName }
+) {
+  run(command, options);
+  if (runCommands) {
+    execSync("git add .");
+    execSync(`git commit -m "${command}"`);
+  }
+}
+
+function run(command: string, options: ExecSyncOptions = { cwd: projectName }) {
+  console.log("\n--------------------------------------------");
+  console.log(command);
+
+  if (runCommands) {
+    execSync(command, options);
+  }
 }
