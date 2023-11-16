4e46a478a8fa64b95e163622184f2df049950db0
ersimont <8042088+ersimont@users.noreply.github.com>
2023-11-15 20:11:29 -0500

    npx ng generate web-worker app

--- the-project/src/app/app.component.ts
+++ the-project/src/app/app.component.ts
@@ -12,3 +12,15 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'the-project';
 }
+
+if (typeof Worker !== 'undefined') {
+  // Create a new
+  const worker = new Worker(new URL('./app.worker', import.meta.url));
+  worker.onmessage = ({ data }) => {
+    console.log(`page got message: ${data}`);
+  };
+  worker.postMessage('hello');
+} else {
+  // Web Workers are not supported in this environment.
+  // You should add a fallback so that your program still executes correctly.
+}
