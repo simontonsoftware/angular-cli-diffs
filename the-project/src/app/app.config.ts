112baaad182a0bbc0337234c74abf5f3121bcc5e
ersimont <8042088+ersimont@users.noreply.github.com>
2024-04-16 18:29:45 -0400

    npx ng add @angular/pwa --interactive=false --skip-confirmation=true

--- the-project/src/app/app.config.ts
+++ the-project/src/app/app.config.ts
@@ -1,6 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimationsAsync()]
+  providers: [provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
