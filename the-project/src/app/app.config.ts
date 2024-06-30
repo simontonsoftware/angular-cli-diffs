216fdef0fa42eca8dab920b8af9f002d14b69fd3
ersimont <8042088+ersimont@users.noreply.github.com>
2024-06-29 21:51:52 -0400

    npx ng add @angular/material@18.0 --theme=custom --typography --interactive=false --skip-confirmation=true

--- the-project/src/app/app.config.ts
+++ the-project/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideAnimationsAsync()]
 };
