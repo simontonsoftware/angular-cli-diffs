5b80f9ebfde518c4b8fd8d965d904c9ca28278e6
ersimont <8042088+ersimont@users.noreply.github.com>
2024-07-02 21:49:48 -0400

    npx ng add @angular/pwa --interactive=false --skip-confirmation=true

--- the-project/src/app/app.config.ts
+++ the-project/src/app/app.config.ts
@@ -1,6 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })]
 };
