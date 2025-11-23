6a91ee5ec51568924a2607bc21c68d28d069203e
ersimont <8042088+ersimont@users.noreply.github.com>
2025-11-23 09:58:47 -0500

    npx ng generate application the-application --routing=false --skip-install --interactive=false

---
+++ the-project/projects/the-application/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('the-application');
+}
