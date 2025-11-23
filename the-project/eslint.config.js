2a00bd167efb5c12ce90532964a13586346c8f13
ersimont <8042088+ersimont@users.noreply.github.com>
2025-11-23 09:58:40 -0500

    npx ng add angular-eslint --interactive=false --skip-confirmation=true

---
+++ the-project/eslint.config.js
@@ -0,0 +1,27 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const { defineConfig } = require("eslint/config");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+
+module.exports = defineConfig([
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.stylistic,
+      angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {},
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      angular.configs.templateRecommended,
+      angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+]);
