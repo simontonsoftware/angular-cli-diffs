74ee2c38cf671109cab7d8da692f70105ce80f74
Eric Simonton <ersimont@gmail.com>
2026-03-17 07:44:22 -0400

    specify material minor version

--- generate-version.ts
+++ generate-version.ts
@@ -48,9 +48,8 @@ if (flags.has("-lib")) {
   );
 }
 if (flags.has("-mat")) {
-  const extraArgs = flags.has("-noApp") ? `--project=${applicationName}` : "";
   runAndCommit(
-    `npx ng add @angular/material@${majorVersion} --theme=custom ${extraArgs} --interactive=false --skip-confirmation=true`
+    `npx ng add @angular/material@${majorVersion}.${minorVersion} ${flags.has("-noApp") ? `--project=${applicationName}` : ""} --theme=custom --interactive=false --skip-confirmation=true`
   );
 }
 if (flags.has("-pwa")) {
