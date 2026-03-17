edc1c9fd5e10b8faed44f84537a2e13261a67815
Eric Simonton <ersimont@gmail.com>
2026-03-17 08:15:52 -0400

    npx ng generate library the-library --test-runner=karma --skip-install --interactive=false

---
+++ the-project/projects/the-library/src/lib/the-library.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TheLibrary } from './the-library';
+
+describe('TheLibrary', () => {
+  let component: TheLibrary;
+  let fixture: ComponentFixture<TheLibrary>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TheLibrary],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TheLibrary);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
